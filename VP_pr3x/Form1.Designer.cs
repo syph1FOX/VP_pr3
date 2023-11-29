8e330c0e9a76f35f1f565158d92a58b9f09e623a
syph1FOX <par-kir@mail.ru>
2023-11-29 22:43:56 +0300

    Добавьте файлы проекта.

---
+++ VP_pr3x/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace VP_pr3x
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Грызуны");
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.treeView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(782, 553);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.treeView1.Location = new System.Drawing.Point(3, 3);
+            this.treeView1.MinimumSize = new System.Drawing.Size(385, 547);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node0";
+            treeNode1.Text = "Грызуны";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.treeView1.Size = new System.Drawing.Size(385, 547);
+            this.treeView1.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.textBox1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.button1, 1, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(394, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(385, 547);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.tableLayoutPanel2.SetColumnSpan(this.label1, 2);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.MinimumSize = new System.Drawing.Size(379, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(379, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Поиск";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(3, 498);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 15);
+            this.textBox1.MinimumSize = new System.Drawing.Size(282, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(282, 34);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(291, 497);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
+            this.button1.MinimumSize = new System.Drawing.Size(91, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ VP_pr3x/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Net.Http.Headers;
+
+namespace VP_pr3x
+{
+
+    public partial class Form1 : Form
+    {
+        List<GNode> gList = null;
+        JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+            WriteIndented = true
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ImageList images = new ImageList();
+            //изображения грызунов
+            images.Images.Add(Image.FromFile(Directory.GetCurrentDirectory() + "\\img\\polevka.jpg"));
+            images.Images.Add(Image.FromFile(Directory.GetCurrentDirectory() + "\\img\\belka.jpg"));
+            images.Images.Add(Image.FromFile(Directory.GetCurrentDirectory() + "\\img\\kapibara.jpg"));
+            images.Images.Add(Image.FromFile(Directory.GetCurrentDirectory() + "\\img\\shinshila.jpg"));
+            images.Images.Add(Image.FromFile(Directory.GetCurrentDirectory() + "\\img\\tushkanchik.jpg"));
+            images.Images.Add(Image.FromFile(Directory.GetCurrentDirectory() + "\\img\\dikobraz.jpg"));
+            images.Images.Add(Image.FromFile(Directory.GetCurrentDirectory() + "\\img\\homyak.jpg"));
+            //первое изображение
+            images.Images.Add(Image.FromFile(Directory.GetCurrentDirectory() + "\\img\\megakapibara.jpg"));
+            using (StreamReader fstream = new StreamReader(Directory.GetCurrentDirectory() + "\\glist.json"))
+            {
+                GNode g = new GNode();
+                gList = new List<GNode>();
+                string text = fstream.ReadToEnd();
+                string[] json = text.Split(new char[] { '{', '}' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string obj in json)
+                {
+                    string wrap = "{" + obj + "}";
+                    g = JsonSerializer.Deserialize<GNode>(wrap);
+                    gList.Add(g);
+                }
+
+            }
+
+            //добавление семейств грызунов(при первом запуске). можно еще добавить при желании
+            //gList = new List<GNode>();
+            //GNode temp;
+            //temp = new GNode("Полевки", "Мелкие вредители", "Повсюду", Directory.GetCurrentDirectory()+"\\img\\polevka.jpg", 2, 15);
+            //gList.Add(temp);
+            //temp = new GNode("Белки", "Пух", "Лес", Directory.GetCurrentDirectory() + "\\img\\belka.jpg", 3, 10);
+            //gList.Add(temp);
+            //temp = new GNode("Капибара", "Милейшие создания", "Оазисы", Directory.GetCurrentDirectory() + "\\img\\kapibara.jpg", 10, 65);
+            //gList.Add(temp);
+            //temp = new GNode("Шиншиллы", "Милашки", "Южная  Америка", Directory.GetCurrentDirectory() + "\\img\\shinshila.jpg", 5, 10);
+            //gList.Add(temp);
+            //temp = new GNode("Тушканчики", "Микро кенгурушки", "Азия и Северная Африка", Directory.GetCurrentDirectory() + "\\img\\tushkanchik.jpg", 6, 5);
+            //gList.Add(temp);
+            //temp = new GNode("Дикобразы", "Колючки", "Южная и Северная Америка", Directory.GetCurrentDirectory() + "\\img\\dikobraz.jpg", 15, 18);
+            //gList.Add(temp);
+            //temp = new GNode("Хомяки", "Пухлячки", "Повсюду", Directory.GetCurrentDirectory() + "\\img\\homyak.jpg", 3, 10);
+            //gList.Add(temp);
+            int i = 0, j;
+            this.treeView1.ImageList = images;
+            this.treeView1.Nodes[0].ImageIndex = images.Images.Count - 1;
+            this.treeView1.Nodes[0].SelectedImageIndex = images.Images.Count - 1;
+            foreach (var g in gList)
+            {
+                j = 0;
+                TreeNode treeNode = new TreeNode(g.name);
+                treeNode.Name = g.name;
+                treeNode.ImageIndex = i;
+                treeNode.SelectedImageIndex = i;
+                this.treeView1.Nodes[0].Nodes.Add(treeNode);
+                this.treeView1.Nodes[0].Nodes[i].Nodes.Add("","Описание", i, i);
+                this.treeView1.Nodes[0].Nodes[i].Nodes[j++].Nodes.Add("", g.description, i, i);
+                this.treeView1.Nodes[0].Nodes[i].Nodes.Add("", "Среда обитания", i, i);
+                this.treeView1.Nodes[0].Nodes[i].Nodes[j++].Nodes.Add("", g.habitat, i, i);
+                this.treeView1.Nodes[0].Nodes[i].Nodes.Add("", "Длительность жизни", i, i);
+                if(g.maxAge > 4)
+                    this.treeView1.Nodes[0].Nodes[i].Nodes[j++].Nodes.Add("", g.maxAge.ToString() + " лет", i, i);
+                else
+                    this.treeView1.Nodes[0].Nodes[i].Nodes[j++].Nodes.Add("", g.maxAge.ToString() + " года", i ,i);
+                this.treeView1.Nodes[0].Nodes[i].Nodes.Add("", "Максимальный вес", i, i);
+                this.treeView1.Nodes[0].Nodes[i].Nodes[j++].Nodes.Add("", g.maxWeight.ToString() + " кг", i, i);
+                this.treeView1.Nodes[0].Nodes[i].Nodes.Add("", "Изображение", i, i);
+                this.treeView1.Nodes[0].Nodes[i].Nodes[j++].Nodes.Add("", g.path, i, i);
+                i++;
+            }
+            this.treeView1.Nodes[0].Expand();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (gList == null) { return; }
+            string path = Directory.GetCurrentDirectory() + "\\glist.json";
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            using (FileStream fstream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                foreach(var g in gList)
+                {
+                    JsonSerializer.Serialize<GNode>(fstream, g, options);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string key = this.textBox1.Text;
+            if (key == string.Empty)
+                return;
+            TreeNode[] temp = this.treeView1.Nodes[0].Nodes.Find(key, true);
+            if (temp.Length > 0)
+            {
+                this.treeView1.Nodes[0].Collapse(false);
+                this.treeView1.Nodes[0].Expand();
+                temp[0].Expand();
+                this.textBox1.Text = string.Empty;
+                return;
+            }
+            MessageBox.Show("Ничего не найдено", "Ошибка поиска");
+        }
+    }
+
+    [Serializable]
+    class GNode
+    {
+        public string name { get; set; }
+        public string description {  get; set; }
+
+        public string habitat {  get; set; }
+
+        public string path {  get; set; }
+        
+
+        public int maxAge { get; set; }
+
+        public int maxWeight { get; set; }
+        public GNode()
+        {
+        }
+        public GNode(string name, string description, string habitat, string path, int maxAge, int maxWeight)
+        {
+            this.name = name;
+            this.description = description;
+            this.habitat = habitat;
+            this.path = path;
+            this.maxAge = maxAge;
+            this.maxWeight = maxWeight;
+        }
+    }
+}
